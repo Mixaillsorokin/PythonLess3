ae5b545fa29685c5b66945f13aa75d11849704bf
Sorokin Mikhail <ip.sorokin1738@mail.ru>
2022-10-20 19:11:02 +0300

    Added code in Task2

--- Task2.py
+++ Task2.py
@@ -5,3 +5,17 @@
 
 # - [2, 3, 4, 5, 6] => [12, 15, 16];
 # - [2, 3, 5, 6] => [12, 15]
+
+Spisok=[2,3,4,5,6,]
+compList=[]
+ind=0
+comp=0
+for i in range(len(Spisok)):
+    if i < len(Spisok)/2:
+        ind=ind-1
+        comp=Spisok[i]*Spisok[ind]
+        i+=1
+        compList.append(comp)
+
+
+print(f'{Spisok} => {compList}')
